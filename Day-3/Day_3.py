e3dc0fbccbf7b20a6c9993943e5b21438d3383f7
trendswave <paulchinedumicheal@gmail.com>
2024-01-12 17:17:13 +0100

    Day 3 of  150

---
+++ Day-3/Day_3.py
@@ -0,0 +1,30 @@
+'this function creates an instance'
+def do_create(self, arg):
+        'Creates new instance'
+        args = parse(arg)
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in classes:
+            print("** class doesn't exist **")
+        else:
+            obj = classes[args[0]]()
+            obj.save()
+            print(obj.id)    
+
+'the function that deletes an instance'
+def do_destroy(self, arg):
+        'Deletes instance of <class> <id>'
+        args = parse(arg)
+        if not args or len(args) == 0 or args[0] == "":
+            print("** class name missing **")
+        elif args[0] not in classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2 or args[1] == "":
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            if key in storage.all():
+                del storage.all()[key]
+                storage.save()
+            else:
+                print("** no instance found **")
