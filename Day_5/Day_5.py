88b35bbcfa5d1c3580e35290401f85a661177786
paul micheal <121412675+trendswave@users.noreply.github.com>
2024-01-14 20:26:44 +0100

    data structure: the beauty  of swapping variables(packing and unpacking)

---
+++ Day_5/Day_5.py
@@ -0,0 +1,11 @@
+# Initialize variables
+a = 89
+b = 10
+
+# Swap the values of 'a' and 'b' using tuple packing and unpacking
+# This is a concise and Pythonic way to perform variable swapping
+a, b = b, a
+
+# Print the swapped values
+print("a={:d} - b={:d}".format(a, b))
+
