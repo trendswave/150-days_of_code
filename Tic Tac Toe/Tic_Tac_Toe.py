c01c89c798083e4ccf081c1a65e7c9649578ddcb
trendswave <paulchinedumicheal@gmail.com>
2024-01-29 20:38:36 +0100

    update: show victory

--- Tic Tac Toe/Tic_Tac_Toe.py
+++ Tic Tac Toe/Tic_Tac_Toe.py
@@ -33,3 +33,34 @@ def your_move(board):
 			print("Field already occupied - repeat your input!")
 			continue
 	board[row][col] = 'O' 	# set '0' at the selected square
+
+
+def list_of_free_fields(board):
+	free = []
+	for row in range(3):  # iterate through rows
+		for col in range(3):  # iterate through columns
+			if board[row][col] not in ['O', 'X']:  # free cell?
+				free.append((row, col))  # append new tuple to the list
+	return free
+
+
+def victory_for(board, sgn):
+	if sgn == "X":  # First player
+		who = 'me'  # The computer
+	elif sgn == "O":  # Second player
+		who = 'you'  # yes - it's our side
+	else:
+		who = None
+	cross1 = cross2 = True  # for diagonals
+	for rc in range(3):
+		if board[rc][0] == sgn and board[rc][1] == sgn and board[rc][2] == sgn:  # check row rc
+			return who
+		if board[0][rc] == sgn and board[1][rc] == sgn and board[2][rc] == sgn:  # check column rc
+			return who
+		if board[rc][rc] != sgn:  # check 1st diagonal
+			cross1 = False
+		if board[2 - rc][2 - rc] != sgn:  # check 2nd diagonal
+			cross2 = False
+	if cross1 or cross2:
+		return who
+	return None
