1420b391c936554b3a7458fc9baad9d3d679c82c
trendswave <paulchinedumicheal@gmail.com>
2024-02-02 22:07:50 +0100

    updated day 22

--- Day_22/Day_22.py
+++ Day_22/Day_22.py
@@ -5,3 +5,19 @@ def draw_move(board):
 		this = randrange(cnt)
 		row, col = free[this]
 		board[row][col] = 'X'
+board = [ [3 * j + i + 1 for i in range(3)] for j in range(3) ] 
+board[1][1] = 'X' # set first 'X' in the middle
+free = make_list_of_free_fields(board)
+human_turn = True # which turn is it now?
+while len(free):
+	display_board(board)
+	if human_turn:
+		enter_move(board)
+		victor = victory_for(board,'O')
+	else:	
+		draw_move(board)
+		victor = victory_for(board,'X')
+	if victor != None:
+		break
+	human_turn = not human_turn		
+	free = make_list_of_free_fields(board)
