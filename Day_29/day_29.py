3f1605d7573e49fc761193d75efae441f00ca47e
trendswave <paulchinedumicheal@gmail.com>
2024-02-07 23:28:00 +0100

    A win for super Eagles

---
+++ Day_29/day_29.py
@@ -0,0 +1,7 @@
+team = input("Enter Country name: ")
+
+if team.lower() == "Nigeria":
+    print("Congratulations your country is  through to the final of the AFCON Cup!")
+else:
+    print(
+        f"Good effort, {team}! Try your best in the 3rd place and win the bronze!")
