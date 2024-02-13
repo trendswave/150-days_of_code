165336e16b96a7b1e0b9ea8e0a5f9aa699601bf4
trendswave <paulchinedumicheal@gmail.com>
2024-02-13 23:00:28 +0100

    updated day 33

---
+++ Day_33/day_33.js
@@ -0,0 +1,10 @@
+const person = {
+  name: ["Bob", "Smith"],
+  age: 32,
+  bio: function () {
+    console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+  },
+  introduceSelf: function () {
+    console.log(`Hi! I'm ${this.name[0]}.`);
+  },
+};
